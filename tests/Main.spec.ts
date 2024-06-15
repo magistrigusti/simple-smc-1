20b82112f62bb0372c14b7ec6942446d3573cd9b
Magistry <Magistrigusti@yandex.ru>
2024-06-16 00:05:45 +0300

    0.1

--- tests/Main.spec.ts
+++ tests/Main.spec.ts
@@ -1,4 +1,4 @@
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract, printTransactionFees } from '@ton/sandbox';
 import { Cell, toNano } from '@ton/core';
 import { Main } from '../wrappers/Main';
 import '@ton/test-utils';
@@ -42,9 +42,92 @@ describe('Main', () => {
     });
 
     it('should send funds to receiver', async () => {
+        const userBalanceBefore = await user.getBalance();
+        const receiverBalanceBefore = await receiver.getBalance();
         const sendFundsResult = await main.sendFunds(user.getSender(), toNano('1'));
         expect(sendFundsResult.transactions).toHaveTransaction({
-            
-        })
+            from: user.address,
+            to: main.address,
+            success: true,
+            outMessagesCount: 1,
+            op: 0x6f074817,
+            value: toNano('1')
+        });
+
+        expect(sendFundsResult.transactions).toHaveTransaction({
+            from: main.address,
+            to: receiver.address,
+            success: true,
+            value: toNano('1')
+        });
+
+        const userBalanceAfter = await user.getBalance();
+        const receiverBalanceAfter = await receiver.getBalance();
+
+        expect(userBalanceAfter).toBeLessThan(userBalanceBefore);
+        expect(receiverBalanceAfter).toBeGreaterThan(receiverBalanceBefore);
+
+        printTransactionFees(sendFundsResult.transactions);
     });
+
+    it('should throw 100 in case msg_value is less than 1 TON', async () => {
+        const sendFundsResult = await main.sendFunds(user.getSender(), toNano('0.1'));
+        expect(sendFundsResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: main.address,
+            success: false,
+            op: 0x6f074817,
+            value: toNano('0.1'),
+            exitCode: 100,
+        });
+
+        expect(sendFundsResult.transactions).toHaveTransaction ({
+            from: main.address,
+            to: user.address,
+            inMessageBounced: true
+        });
+
+        printTransactionFees(sendFundsResult.transactions);
+    });
+
+    it('should lock and unlock', async () => {
+        expect(await main.getIsLocked()).toEqual(0);
+        const sendLockResult = await main.sendLock(admin.getSender());
+        expect(sendLockResult.transactions).toHaveTransaction({
+            from: admin.address,
+            to: main.address,
+            success: true,
+            op: 0x878f9b0e
+        });
+
+        expect(await main.getIsLocked()).toEqual(1);
+
+        const sendFundsResult = await main.sendFunds(user.getSender(), toNano('0.1'));
+        expect(sendFundsResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: main.address,
+            success: false,
+            exitCode: 99
+        });
+
+        const sendUnlockWrongSenderResult = await main.sendUnlock(user.getSender());
+        expect(sendUnlockWrongSenderResult.transactions).toHaveTransaction({
+            from: user.address,
+            to: main.address,
+            success: false,
+            exitCode: 101
+        });
+
+        expect(await main.getIsLocked()).toEqual(1);
+
+        const sendUnlockResult = await main.sendUnlock(admin.getSender());
+        expect(sendUnlockResult.transactions).toHaveTransaction({
+            from: admin.address,
+            to: main.address,
+            success: true 
+        });
+
+        expect(await main.getIsLocked()).toEqual(0);
+    });
+
 });
--- wrappers/Main.ts
+++ wrappers/Main.ts
@@ -1,4 +1,4 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode, toNano } from '@ton/core';
 
 export type MainConfig = {
     receiver: Address;
@@ -42,5 +42,30 @@ export class Main implements Contract {
                 .storeUint(0x6f074817, 32)
             .endCell(),
         });
+    };
+
+    async sendLock(provider: ContractProvider, via: Sender) {
+        await provider.internal(via, {
+            value: toNano('0.02'),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                    .storeUint(0x878f9b0e, 32)
+                .endCell(),
+        });
+    }
+
+    async sendUnlock(provider: ContractProvider, via: Sender) {
+        await provider.internal(via, {
+            value: toNano('0.02'),
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                    .storeUint(0x6ae4b0ef, 32)
+                .endCell(),
+        });
+    }
+
+    async getIsLocked(provider: ContractProvider): Promise<number> {
+        const res = (await provider.get('get_is_locked', [])).stack;
+        return res.readNumber();
     }
 }
